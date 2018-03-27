e4dc20b277d07dc4a0f2625e2ed807be4a29345a
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-03-27 13:48:57 -0700

    Respond to comments.

--- src/RahaBlockchain.ts
+++ src/RahaBlockchain.ts
@@ -2,15 +2,23 @@ import { Buffer } from 'buffer';
 import * as bs58 from 'bs58';
 import fetch, { Response } from 'node-fetch';
 import StellarSdk from 'stellar-sdk';
+import * as url from 'url';
 
 import { IPFS_ENDPOINT, IPFS_SHA_256_LEN_32_PREFIX, STELLAR_ENDPOINT, RAHA_IO_STELLAR_PUBLIC_KEY } from './constants';
 
 /**
- * Note: TransactionMetadata is undocumented and quite ugly to interact with. Mark had to look into the SDK source to do this.
+ * Note: TransactionMetadata is undocumented and quite ugly to interact with. Mark had to look into the SDK source to do
  * TODO: Can we use documented features/do this in an easier to understand and less brittle way?
  */
+function getFirstOperationAttributesFromTransactionMeta(transactionMeta) {
+    return transactionMeta._value[0]._attributes.changes[0]._value._attributes.data._value._attributes
+}
+
+/**
+ * Return the name of a block from transaction metadata.
+ */
 function getBlockNameFromTransactionMeta(transactionMeta) {
-    return transactionMeta._value[0]._attributes.changes[0]._value._attributes.data._value._attributes.dataName;
+    return getFirstOperationAttributesFromTransactionMeta(transactionMeta).dataName;
 }
 
 /**
@@ -18,7 +26,7 @@ function getBlockNameFromTransactionMeta(transactionMeta) {
  * See note on TransactionMetadata above.
  */
 function getMultiHashFromTransactionMeta(transactionMeta) {
-    const charCode = transactionMeta._value[0]._attributes.changes[0]._value._attributes.data._value._attributes.dataValue;
+    const charCode = getFirstOperationAttributesFromTransactionMeta(transactionMeta).dataValue;
     return String.fromCharCode.apply(null, charCode);
 }
 
@@ -42,16 +50,22 @@ function memoToMultiHash(memoHashB64) {
     return bs58.encode(Uint8Array.from(resBytes));
 }
 
+interface BlockMetaData {
+    readonly multiHash: string,
+    readonly timeStr: string,
+    readonly stellarTxId: string,
+}
+
 /**
  * Transform a Stellar transaction into an object describing the Raha blockchain block to which it points.
  * Return null if the transaction does not describe a block.
  */
-function stellarTxToBlockMetaData(stellarTx) {
+function stellarTxToBlockMetaData(stellarTx): BlockMetaData|void {
     const transactionMeta = getTransactionMetaFromTransaction(stellarTx);
     const blockName = getBlockNameFromTransactionMeta(transactionMeta);
     if (!blockName || !blockName.startsWith('block-')) {
         // This is not a blockchain transaction.
-        return null;
+        return undefined;
     }
     const stellarTxId = stellarTx.id;
     const timeStr = stellarTx.created_at;
@@ -59,54 +73,40 @@ function stellarTxToBlockMetaData(stellarTx) {
     return { multiHash, stellarTxId, timeStr };
 }
 
-class RahaBlockchain {
-    horizonServer;
-    transactions;
-    blockMetadata;
-    blocks;
-
-    constructor() {
-        this.horizonServer = new StellarSdk.Server(STELLAR_ENDPOINT);
-    }
-
-    /**
-     * Return all transactions associated with the Raha account from the Stellar blockchain ordered chronologically.
-     */
-    async getTransactions() {
-        if (this.transactions === undefined) {
-            this.transactions = (await this.horizonServer.transactions()
-                .forAccount(RAHA_IO_STELLAR_PUBLIC_KEY)
-                .order('desc')
-                .call()).records;
-        }
-        return this.transactions;
-    }
-
-    /**
-     * Return metadata about all IPFS blocks in the Raha blockchain.
-     */
-    async getBlockMetadata() {
-        if (this.blockMetadata === undefined) {
-            const transactions = await this.getTransactions();
-            this.blockMetadata = transactions.map(stellarTxToBlockMetaData).filter(x => x !== null);
-        }
-        return this.blockMetadata;
-    }
-
-    /**
-     * Return the entire Raha blockchain as a list of objects ordered chronologically.
-     */
-    async getBlocks() {
-        if (this.blocks === undefined) {
-            const blockMetaData = await this.getBlockMetadata();
-            const multiHashes = blockMetaData.map((metaData) => metaData.multiHash);
-            const blocksData = await Promise.all<Response>(
-                multiHashes.map((multiHash) => fetch(IPFS_ENDPOINT + multiHash)));
-            this.blocks = await Promise.all(
-                blocksData.map((blockData) => blockData.json()));
-        }
-        return this.blocks;
-    }
+/**
+ * Return all transactions associated with the Raha account from the Stellar blockchain ordered chronologically.
+ * TODO: Documentation doesn't specify the current page limit. Add paging support.
+ */
+async function getTransactions() {
+    const horizonServer = new StellarSdk.Server(STELLAR_ENDPOINT);
+    return (await horizonServer.transactions()
+        .forAccount(RAHA_IO_STELLAR_PUBLIC_KEY)
+        .order('desc')
+        .call()).records;
 }
 
-export default RahaBlockchain;
+/**
+ * Return metadata about all IPFS blocks in the Raha blockchain.
+ */
+async function getBlockMetadata() {
+    const transactions = await getTransactions();
+    return transactions.map(stellarTxToBlockMetaData).filter(x => x) as Array<BlockMetaData>;
+}
+
+/**
+ * Return the entire Raha blockchain as a list of objects ordered chronologically.
+ */
+async function getBlocks() {
+    const blockMetaData = await getBlockMetadata();
+    const multiHashes = blockMetaData.map((metaData) => metaData.multiHash);
+    const blocksData = await Promise.all<Response>(
+        multiHashes.map((multiHash) => fetch(url.resolve(IPFS_ENDPOINT, multiHash))));
+    return await Promise.all(
+        blocksData.map((blockData) => blockData.json()));
+}
+
+export {
+    getTransactions,
+    getBlockMetadata,
+    getBlocks,
+};
