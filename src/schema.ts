62e4439d87c94dd8d93bfe00f32660cd1a7b4dc8
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-03-27 20:38:52 -0700

    Add common schema definition.

---
+++ src/schema.ts
@@ -0,0 +1,66 @@
+/**
+ * Raha Blockchain Schema Version 1.
+ * TODO: Define the schema using an InterfaceDefinitionLanguage like Thrift
+ * that can then be compiled to interfaces in other languages.
+ */
+
+const BLOCKCHAIN_VERSION_NO = 1;
+
+interface OperationData {}
+
+interface RequestInvite extends OperationData {
+    full_name: string,
+    to_mid: string,
+    video_url: string,
+}
+
+interface Trust extends OperationData {
+    to_mid: string,
+}
+
+interface Operation {
+    sequence: number,
+    op_code: string,
+    creator_mid: string,
+    data: OperationData,
+}
+
+/**
+ * The interface of the block as stored in IPFS.
+ */
+interface IpfsBlock {
+    sequence: number,
+    origin_created: string|void,
+    version: 1,
+    prev_version_block: string|void,
+    prev_hash: string,
+    operations: Array<Operation>,
+}
+
+/**
+ * Metadata we care about from the Stellar transaction.
+ */
+interface StellarMetadata {
+    multiHash: string,
+    timeStr: string,
+    stellarTxId: string,
+}
+
+/**
+ * A block in the blockchain.
+ * This can be considered a virtual data structured stored across both Stellar and IPFS.
+ */
+interface Block {
+    metadata: StellarMetadata,
+    data: IpfsBlock,
+}
+
+export {
+    BLOCKCHAIN_VERSION_NO,
+    Block,
+    IpfsBlock,
+    StellarMetadata,
+    Operation,
+    RequestInvite,
+    Trust,
+};
