c14ce74c90146c35a96a630106393e9aebfb0aec
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-04-01 16:41:09 -0700

    Move main to single try-catch block.

--- src/scripts/createTransaction.ts
+++ src/scripts/createTransaction.ts
@@ -7,37 +7,34 @@ import RahaStellar, { getNewTestAccount } from '../RahaStellar';
 const modes = ['test', 'prod'];
 
 function errorOnUsage() {
-    console.log('Usage is: node uploadBlock.ts [test|prod] [multiHash] [secretKey (optional if testing)]')
-    process.exit(1);
+    throw new Error('Usage is: node uploadBlock.ts [test|prod] [multiHash] [secretKey (optional if testing)]');
 }
 
 async function main(args) {
-    // Verify existence of isTest and multiHash arguments
-    if (args.length < 4) errorOnUsage();
-
-    const mode = process.argv[2];
-    if (!modes.includes(mode)) errorOnUsage();
-    const isTest = mode === 'test';
-
-    const multiHash = process.argv[3];
-
-    let publicKey;
-    let secretKey;
-    if (args.length < 5) {
-        if (isTest) {
-            const keyPair = await getNewTestAccount();
-            console.log(`New test public key is ${keyPair.publicKey()}.`);
-            secretKey = keyPair.secret();
-        } else {
-            errorOnUsage();
-        }
-    } else {
-        secretKey = args[4];
-    }
-
     try {
+        // Verify existence of isTest and multiHash arguments
+        if (args.length < 4) errorOnUsage();
+
+        const mode = process.argv[2];
+        if (!modes.includes(mode)) errorOnUsage();
+        const isTest = mode === 'test';
+
+        const multiHash = process.argv[3];
+
+        let publicKey;
+        let secretKey;
+        if (args.length < 5) {
+            if (isTest) {
+                const keyPair = await getNewTestAccount();
+                console.log(`New test public key is ${keyPair.publicKey()}.`);
+                secretKey = keyPair.secret();
+            } else {
+                errorOnUsage();
+            }
+        } else {
+            secretKey = args[4];
+        }
         console.log(await new RahaStellar(isTest).createRahaBlockchainTransaction(multiHash, secretKey));
-        process.exit(0);
     } catch (err) {
         if (err.name === 'BadResponseError') {
             console.error(err.message);
@@ -47,6 +44,7 @@ async function main(args) {
         }
         process.exit(1);
     }
+    process.exit(0);
 }
 
 main(process.argv);
