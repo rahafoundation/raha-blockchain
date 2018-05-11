6473da62474cf17ee7d8419bde6fd6a6bb632248
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-05-11 14:54:56 -0700

    Comment improvements.

--- src/schema.ts
+++ src/schema.ts
@@ -3,7 +3,9 @@ import { Block } from "./schema/blockchain/version_01";
 export const BLOCKCHAIN_VERSION_NO = 1;
 
 /**
- * Metadata we care about from the Stellar transaction.
+ * Block Metadata we care about from the Stellar transaction.
+ * This allows us to verify the block data and creation time by storing this
+ * information in an external trusted blockchain.
  */
 export interface StellarMetadata {
   multiHash: string;
@@ -12,7 +14,7 @@ export interface StellarMetadata {
 }
 
 /**
- * A block in the blockchain.
+ * A block in the blockchain composed of the raw block data and Stellar Metadata.
  * This can be considered a virtual data structured stored across both Stellar and IPFS.
  */
 export interface VirtualBlock {
